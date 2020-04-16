19addabc852b67fafe065a8b5b5a546553ef6c13
lxc <761360972@qq.com>
2020-04-16 10:29:09 +0800

    短信验证

---
+++ swiper/config.py
@@ -0,0 +1,13 @@
+"""
+第三方配置
+"""
+
+HY_SMS_URL = 'https://106.ihuyi.com/webservice/sms.php?method=Submit'
+HY_SMS_PARAMS = {
+    'account': 'C91565239',
+    'password': 'facb86fcc3c10c3b98366411a7663b42',
+    'content': '您的验证码是：%s.请不要把验证码泄露给其他人',
+    'mobile': None,
+    'format': 'json',
+
+}
