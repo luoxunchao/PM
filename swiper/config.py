70c14c5601a5dcfb5fdb5bec448257aa7075bd34
lxc <761360972@qq.com>
2020-04-21 12:02:18 +0800

    开发完成了接受验证码的接口

--- swiper/config.py
+++ swiper/config.py
@@ -6,8 +6,20 @@ HY_SMS_URL = 'https://106.ihuyi.com/webservice/sms.php?method=Submit'
 HY_SMS_PARAMS = {
     'account': 'C91565239',
     'password': 'facb86fcc3c10c3b98366411a7663b42',
-    'content': '您的验证码是：%s.请不要把验证码泄露给其他人',
+    'content': '您的验证码是：%s。请不要把验证码泄露给其他人。',
     'mobile': None,
     'format': 'json',
-
 }
+
+
+# =========================云之讯=============================
+# HY_SMS_URL = 'https://open.ucpaas.com/ol/sms/{function}'
+# HY_SMS_PARAMS = {
+#     'appid': 'cfdbb259caa84cc8ab1377814f92f5a0',
+#     'sid': '08ec28e7eb133affc35bd9e145e60248',
+#     'token': '336b4f61a4e3dac5599480895e303cf1',
+#     'templateid': '539606',
+#     'param': '【云之讯】您的验证码是：%s.请不要把验证码泄露给其他人【自用测试】',
+#     'mobile': None,
+#     'format': 'json',
+# }
---
+++ worker/__init__.py
@@ -0,0 +1,26 @@
+import os
+from celery import Celery
+
+# 设置环境变量，加载 Django 的 settings
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'swiper.settings')
+
+# 创建 Celery Application
+
+celery_app = Celery('swiper')
+celery_app.config_from_object('worker.config')
+celery_app.autodiscover_tasks()
+
+
+def call_by_worker(func):
+    '''将任务在 Celery 中异步执行'''
+    task = celery_app.task(func)
+    return task.delay
+
+
+# @celery_app.task
+# def add(x,y):
+#     return x+y
+#
+# add = celery_app.task(add)
+# add(x, y)
